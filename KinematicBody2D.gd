9841ce41a7dbf89828d925e42afe78aa918fe9de
EljakimHerrewijnen <e.herrewijnen@gmail.com>
2020-10-03 14:49:11 +0200

    Added interaction map

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -7,6 +7,7 @@ onready var background_map  = get_node("/root/Map1/background")
 onready var player = get_node("/root/Map1/Player")
 onready var cell_size = background_map._get_cell_size()
 onready var plants_map = get_node("/root/Map1/interaction_map")
+onready var interaction = get_node("/root/Map1/player_interaction")
 
 var velocity = Vector2()
 
@@ -27,6 +28,9 @@ func _physics_process(delta):
 		velocity.x = 0
 		velocity.y = 0
 	move_and_slide(velocity, Vector2(0, -1))
+	if(interaction.get_cell(int(self.position.x / cell_size.x), int(self.position.y / cell_size.y)) == -1):
+		interaction.clear()
+		interaction.set_cell(int(self.position.x / cell_size.x), int(self.position.y / cell_size.y), 0)
 
 #Handles interaction with the map
 func _interaction_process():
