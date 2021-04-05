e35ec3307994f1a1bf8fb13e6415c1650b5902bc
Theamerix <jonathan.herrewijnen@gmail.com>
2021-04-05 23:02:20 +0200

    Added some objects. Changed storage to being in user://storage.db. And cleaned up some mess in the project folder.

--- Menu.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _ready():
-	pass # Replace with function body.
-
-func _on_Btn_PlayGame_pressed():
-	Global.LoadSave()
-	Global.GoToScene("river_intersection_home_2")
-
--- TouchScreenButton.gd
+++
@@ -1,6 +0,0 @@
-extends TouchScreenButton
-
-onready var ShowInventory = Global.ShowInventory
-
-func _input(always):
-	ShowInventory = 1;
