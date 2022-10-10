a7eb33d3b4e04c5b9564e403ace0dde47ab71be4
Jonathan Herrewijnen <jonathan.herrewijnen@herreweb.nl>
2022-10-10 21:15:51 +0200

    Testing commit on herreweb to see if username is right

--- Other/base_tilemap/init.gd
+++ Other/base_tilemap/init.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 func load_scene(map_data):
-	$background.tile_set = load("base_tilemap/tilesets/background_ts.tres")
-	$vegetation.tile_set = load("res://river_intersection_home2.tscn::1")
+	#$background.tile_set = load("base_tilemap/tilesets/background_ts.tres")
+	#$vegetation.tile_set = load("res://river_intersection_home2.tscn::1")
 
 #	for x in range(map_data.width):
 #		for y in range(map_data.height):
