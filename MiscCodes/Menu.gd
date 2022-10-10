79387ea1ddbcac72a12ce34e9d5b24627a5f5a60
Jonathan Herrewijnen <jonathan.herrewijnen@herreweb.nl>
2022-10-10 21:37:48 +0200

    Testing commits for activity tracker

--- MiscCodes/Menu.gd
+++ MiscCodes/Menu.gd
@@ -6,17 +6,6 @@ func _ready():
 func _on_Btn_PlayGame_pressed():
 	Global.LoadSave()
 	Global.GoToScene("res://Other/base_tilemap/base_tilemap.tscn") #Hier kan eventueel de mapnaam van de database ingevoerd worden!
-	
-	#create gamemap on the fly - Obsolete as of 23 08 2021
-#	var game_data = GlobalStructures.base_tilemap.new()
-#	var map_data = [[]]
-#	for x in range(150):
-#		map_data.append([])
-#		for y in range(150):
-#			map_data[x].append(int(rand_range(0, 3)))
-#	game_data.init_map(150, 150, "res://omgeving/Floor.tres",map_data, "res://omgeving/Floor.tres", map_data, "res://omgeving/Floor.tres", map_data, "res://omgeving/Floor.tres", map_data)
-#	var game = load("res://base_tilemap/base_tilemap.tscn").instance()
-#	game.load_scene(game_data)
 #
 #
 #	#save this scene to global scenes
