4d6ac271c06aa22be23220b88ca042e2bea6b9ab
Jonathan Herrewijnen <jonathan.herrewijnen@herreweb.nl>
2022-10-13 17:35:43 +0200

    Fixing bug

--- Global_InGame.gd
+++ Global_InGame.gd
@@ -1,5 +1,5 @@
 extends Node2D
-const gamevariables = "res://MiscScenes/GameVariables.tscn"
+const game_variables = "res://MiscScenes/GameVariables.tscn"
 
 func RenderSpeech(load_on, text):
 	var speech = preload("res://MiscScenes/Speech.tscn").instance()
@@ -8,7 +8,7 @@ func RenderSpeech(load_on, text):
 	load_on.add_child(speech)
 
 func SoundOnInteraction(load_on, sound_name):
-	var audio = preload(gamevariables).instance().get_node("Sound")
-	audio.stream = load("res://Sounds/%s.wav" % sound_name)
+	var audio = preload(game_variables).instance()
+	audio.get_node("Sound").stream = load("res://Sounds/%s.wav" % sound_name)
 	load_on.add_child(audio)
 	Global.Log("Sound playing "+sound_name, 1)
